3ca2e05f7db577e847f451825c2b56ff3e6af0f4
MaximeG44270 <gilbert.maxime01@gmail.com>
2025-03-20 23:18:26 +0100

    ajout footer

---
+++ src/composants/Planete/Light/FooterLight.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterLight: React.FC = () => {
+  return (
+    <footer className="bg-black text-white text-xl font-poppins text-center h-60 py-20 px-10">
+      Footer mettre les planetes avec carousel et liens pour afficher dans main la planète
+    </footer>
+  );
+};
+
+export default FooterLight;
--- src/composants/Planete/Light/Light.tsx
+++ src/composants/Planete/Light/Light.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import FooterLight from './FooterLight';
 import Header from '../Header2/Header';
 
 const Light: React.FC = () => {
   return (
-    <div className="w-full h-screen bg-gray-800 text-white">
+    <div className="flex flex-col min-h-screen bg-gray-800 text-white">
+      {/* ------------------------------------------- Header collé en haut ------------------------------------------- */}
       <Header />
-      <h1 className="text-4xl text-center font-bold">Bienvenue sur la page Côté Lumineux</h1>
+
+      {/* ------------------------------------------- Contenu qui prend l'espace restant ------------------------------------------- */}
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-4xl text-center font-bold">
+          Bienvenue sur la page Côté Lumineux
+        </h1>
+      </main>
+
+      {/* ------------------------------------------- Footer collé en bas ------------------------------------------- */}
+      <FooterLight />
     </div>
   );
 };
